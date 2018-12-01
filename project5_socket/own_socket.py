099852d96e661d1fa5bf4c021f06bde88125cbf2
xiangyiliu18 <xiangyiliu@cs.stonybrook.edu>
2018-12-01 17:27:31 -0500

    add config choice

--- project5_socket/own_socket.py
+++ project5_socket/own_socket.py
@@ -1,12 +1,10 @@
 import socket
 import sys
-import urllib.request
-
+import argparse
 from bs4 import BeautifulSoup
 
-
+# count_page = -1
 class OwnSocket:
-
     def __init__(self, sock=None):
         if sock is None:
             ### AF_INET---> refers to the address family ipv4
@@ -21,10 +19,12 @@ class OwnSocket:
         in a request for the text of the page
     '''
     def connect(self, url):
+        global count_page
         _, _, host, path = url.split('/',3)
         addr = socket.getaddrinfo(host, 80)[0][-1]
         self.sock.connect(addr)
-        sent = self.sock.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+        sent = self.sock.sendall(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+        # count_page++
         if sent == 0:
                 raise RuntimeError("socket connection broken")
         total_data=[]
@@ -40,51 +40,68 @@ class OwnSocket:
         return total_data
 
 
+    def get_response(self,url, filename):  ## one page response
+        response = self.connect(url)
+        response = b''.join(response)
+        all_content = response.decode()
 
-####################################################
-def parse_url(url):
-    print("For manipulating data by using beautifuSoup")
+        header = all_content.split('\r\n\r\n')[0]
+        with open("header.txt", "w") as f:
+            f.write(header)
+            f.close()
 
-
-
+        content = all_content.split('\r\n\r\n')[1]
+        content = content.encode()
+        with open(filename, 'wb') as f:
+            f.write(content)
+            f.close()
 
 ###############################################################
   # 1. The scraping rules can be found in the robots.txt file.  which can be 
   #     found by adding a/robots.txt path to the main domain of the site
 ###############################################################
-def crawl(urls_list,use_agent, choice,max_depth, max_total):
-    ### the total url_list i need to crawl
-    ## custome User_Agent
-    ## choie --: Depth-first, or Breadth-first
-    ## Maximum depth of pages to crawl
-    ## Maximum total number of crawled pages
-    print("crawl")
+
+
+class Crawler:
+    def __init__(self):
+        self.words = []  ### Empty words list
+        self.choice = 'depth' ##Depth-first(depth) / Breadth-first(breadth)
+        self.maxD = 1 ##  max depth of pages to crawl
+        self.maxTotal = 0 ## max total number of pages 
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-u', '--user', nargs='?', type=str, metavar='user_agent', help='must be provide a custom user-agent for use', required=True)
+        parser.add_argument('-c', '--choice', nargs='?', type=str, choices=['depth','breadth'], default='depth', help="Depth-first/Breadth-first choice of crawling", required=False)
+        parser.add_argument('-d', '--depth', nargs='?', type=int, metavar='num', default=0, help="Maximum depth of pages to crawl", required=False)
+        parser.add_argument('-p','--page', nargs='?', type=int, metavar='num', default=0, help='Maximum total number of crawled pages', required= False)
+        sys.args = parser.parse_args()
+    
+    def help(self):
+        parser.print_help()
+
+
+def create_base_dict(words_file):
+    with open(words_file, 'r') as f:
+        words_list=[]
+        for line in f:
+            words_list += line.split()
+        f.close()
+        return words_list
+
 
 
 def main():
+    crawler = Crawler()
     sock=OwnSocket()
-    response = sock.connect("http://songyy.pythonanywhere.com/quotes")
-    response = b''.join(response)
-    all_content = response.decode()
+    sock.get_response("http://songyy.pythonanywhere.com/quotes", "home.html")
+    crawler = Crawler()
+    own_config = {}
+    own_config['user_agent']= sys.args.user  # one user_agent
+    own_config['choice'] = sys.args.choice  # breadth or depth
+    own_config['depth']= sys.args.depth  #  depth of pages to crawling
+    own_config['page'] = sys.args.page  # number of pages to crawled pages
 
-    header = all_content.split('\r\n\r\n')[0]
 
-    content = all_content.split('\r\n\r\n')[1]
 
-    with open("header.txt", "w") as f:
-        f.write(header)
-        f.close()
-## Get html
-    content = content.encode('utf8')
-    with open("home.html", 'wb') as f:
-         f.write(content)
-         f.close()
 
-    # get = urllib.request.urlopen("http://songyy.pythonanywhere.com/quotes")
-    # html = get.read()
-    # print(html)
-    # soup = BeautifulSoup(html,features="")
-    # with open("test.html", "w+") as f:
-    #     f.write(str(soup.html.encode('utf8')))
 
 main()
